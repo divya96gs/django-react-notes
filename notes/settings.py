95d3d68ad462f4326a79c3c688fc570f55bc8f63
Divya <divyags96@gmail.com>
2021-11-27 17:45:21 +0530

    Added gunicorn and django_heroku for heroku deployment

--- notes/settings.py
+++ notes/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 
@@ -141,3 +141,5 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+django_heroku.settings(locals())
